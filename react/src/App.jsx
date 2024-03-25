3477d5584fe22c725c7121ae06bc6b1fd3708a80
tommys0 <149568540+tommys0@users.noreply.github.com>
2024-03-25 09:53:30 +0100

    Project

---
+++ react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MovieList from './components/MovieList.jsx';
+
+function App() {
+    return (
+        <div className="app">
+            <h1>Fav Movies</h1>
+            <MovieList />
+        </div>
+    );
+}
+
+export default App;
